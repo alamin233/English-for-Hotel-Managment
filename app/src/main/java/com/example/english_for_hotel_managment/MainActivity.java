044629753bd78bc39c2e9deb811681a91eeaaacf
alamin233 <alaminhossainnahid@gmail.com>
2020-03-10 17:44:32 -0700

    English for Hotel Management Error Home work

--- app/src/main/java/com/example/english_for_hotel_managment/MainActivity.java
+++ app/src/main/java/com/example/english_for_hotel_managment/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.english_for_hotel_managment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" English for Hotel ");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.yello));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageView = findViewById(R.id.recept);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Reception.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
