89d45195695ae70fccc2fb3da5572fe03aab9210
alamin233 <alaminhossainnahid@gmail.com>
2020-03-15 17:55:52 -0700

    English for Hotel Management Error Home work

---
+++ app/src/main/java/com/example/english_for_hotel_managment/DataModel.java
@@ -0,0 +1,28 @@
+package com.example.english_for_hotel_managment;
+
+/**
+ * Created by anupamchugh on 09/02/16.
+ */
+public class DataModel {
+
+    String englishText;
+    String banglaText;
+
+
+    public DataModel(String englishText, String banglaText) {
+        this.englishText=englishText;
+        this.banglaText=banglaText;
+
+    }
+
+    public String getEnglishText() {
+        return englishText;
+    }
+
+
+    public String getBanglaText() {
+        return banglaText;
+    }
+
+
+}
--- app/src/main/java/com/example/english_for_hotel_managment/MainActivity.java
+++ app/src/main/java/com/example/english_for_hotel_managment/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.english_for_hotel_managment;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Reception.class);
+                Intent intent = new Intent(MainActivity.this,ReceptionWork.class);
                 startActivity(intent);
             }
         });
